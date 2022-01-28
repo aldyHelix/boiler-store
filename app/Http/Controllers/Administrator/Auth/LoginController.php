fada97c8217a8ffaef0fecb1abee78a8b5b04e76
Aldy Satria Gumilar <pilusnarziz3@gmail.com>
2022-01-28 20:41:36 +0700

    login layout metronic

--- app/Http/Controllers/Administrator/Auth/LoginController.php
+++ app/Http/Controllers/Administrator/Auth/LoginController.php
@@ -43,7 +43,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('ladmin::auth.login');
+        return view('auth.login');
     }
 
     /**
--- resources/views/vendor/ladmin/layouts/auth.blade.php
+++ resources/views/vendor/ladmin/layouts/auth.blade.php
@@ -1,40 +1,68 @@
-<x-guest-layout>
-  <x-jet-authentication-card>
-      <x-slot name="logo">
-        @if (config('ladmin.logo'))
-           <img src="{{ config('ladmin.logo') }}" alt="Logo" width="150">
-        @else 
-          {{ config('app.name') }}
-@endif
-      </x-slot>
+<x-auth-layout>
 
-      <x-jet-validation-errors class="mb-4" />
+    <!--begin::Signin Form-->
+    <form method="POST"  action="{{ route('login') }}" class="form w-100" novalidate="novalidate"
+        id="kt_sign_in_form">
+        @csrf
 
-      @if (session('status'))
-          <div class="mb-4 font-medium text-sm text-green-600">
-              {{ session('status') }}
-          </div>
-      @endif
+        <!--begin::Heading-->
+        <div class="text-center mb-10">
+            @if (config('ladmin.logo'))
+                <img src="{{ config('ladmin.logo') }}" alt="Logo" width="150">
+            @else
+                {{ config('app.name') }}
+            @endif
+            <!--begin::Title-->
+            <h1 class="text-dark mb-3">
+                {{ __('Sign In to Sneaker.id ') }}
+            </h1>
+            <!--end::Title-->
+        </div>
+        <!--begin::Heading-->
 
-      <form method="POST" action="{{ route('login') }}">
-          @csrf
+        <!--begin::Input group-->
+        <div class="fv-row mb-10">
+            <!--begin::Label-->
+            <label class="form-label fs-6 fw-bolder text-dark">{{ __('Email') }}</label>
+            <!--end::Label-->
 
-          <div>
-              <x-jet-label for="email" value="{{ __('Email') }}" />
-              <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-          </div>
+            <!--begin::Input-->
+            <input class="form-control form-control-lg form-control-solid" type="email" name="email" autocomplete="off"
+                value="{{ old('email', 'demo@demo.com') }}" required autofocus />
+            <!--end::Input-->
+        </div>
+        <!--end::Input group-->
 
-          <div class="mt-4">
-              <x-jet-label for="password" value="{{ __('Password') }}" />
-              <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-          </div>
+        <!--begin::Input group-->
+        <div class="fv-row mb-10">
+            <!--begin::Wrapper-->
+            <div class="d-flex flex-stack mb-2">
+                <!--begin::Label-->
+                <label class="form-label fw-bolder text-dark fs-6 mb-0">{{ __('Password') }}</label>
+                <!--end::Label-->
+                <!--end::Link-->
+            </div>
+            <!--end::Wrapper-->
 
-              @endif
+            <!--begin::Input-->
+            <input class="form-control form-control-lg form-control-solid" type="password" name="password"
+                autocomplete="off" value="demo" required />
+            <!--end::Input-->
+        </div>
+        <!--end::Input group-->
 
-              <x-jet-button class="ml-4">
-                  {{ __('Log in') }}
-              </x-jet-button>
-          </div>
-      </form>
-  </x-jet-authentication-card>
-</x-guest-layout>
+        <!--begin::Input group-->
+        <div class="fv-row mb-10">
+            <label class="form-check form-check-custom form-check-solid">
+                <input class="form-check-input" type="checkbox" name="remember" />
+                <span class="form-check-label fw-bold text-gray-700 fs-6">{{ __('Remember me') }}
+                </span>
+            </label>
+        </div>
+        <!--end::Input group-->
+
+
+    </form>
+    <!--end::Signin Form-->
+
+</x-auth-layout>
